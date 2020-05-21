cb3dcf2aad51f21593b33058467b0aaf0ee8f74e
wswind <18399096+wswind@users.noreply.github.com>
2020-05-21 10:12:15 +0800

    init

---
+++ Api2/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+            
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+           return "Ok";
+        }
+    }
+}
